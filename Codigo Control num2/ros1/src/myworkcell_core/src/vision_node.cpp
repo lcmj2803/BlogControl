96f7d7b62cea0c128fe30deb9d6ddaa37e387673
Luis Carlos Meron Jaen <106900013+lcmj2803@users.noreply.github.com>
2022-07-04 09:13:52 -0500

    Update vision_node.cpp

--- Codigo Control num2/ros1/src/myworkcell_core/src/vision_node.cpp
+++ Codigo Control num2/ros1/src/myworkcell_core/src/vision_node.cpp
@@ -1,6 +1,4 @@
-/**
-**  Simple ROS Node
-**/
+
 #include <ros/ros.h>
 #include <fake_ar_publisher/ARMarker.h>
 #include <myworkcell_core/LocalizePart.h>
@@ -19,13 +17,13 @@ public:
     void visionCallback(const fake_ar_publisher::ARMarkerConstPtr& msg)
     {
         last_msg_ = msg;
-        //ROS_INFO_STREAM(last_msg_->pose.pose);
+        
     }
 
     bool localizePart(myworkcell_core::LocalizePart::Request& req,
                       myworkcell_core::LocalizePart::Response& res)
     {
-      // Read last message
+      
       fake_ar_publisher::ARMarkerConstPtr p = last_msg_;
       if (!p) return false;
 
@@ -40,17 +38,16 @@ public:
 
 int main(int argc, char* argv[])
 {
-    // This must be called before anything else ROS-related
+   
     ros::init(argc, argv, "vision_node");
 
-    // Create a ROS node handle
+    
     ros::NodeHandle nh;
 
-    // The Localizer class provides this node's ROS interfaces
+    
     Localizer localizer(nh);
 
     ROS_INFO("Vision node starting");
 
-    // Don't exit the program.
     ros::spin();
 }
